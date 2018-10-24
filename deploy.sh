25f8ce5f71b9c37330bb2fc194c2f4c113293c5b
Carlos Santana <csantanapr@apache.org>
2018-10-23 20:36:56 -0400

    allow to use wsk for local openwhisk deployment

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set +x
-WSK_CLI="bx wsk"
+
+WSK_CLI=${WSK_CLI:-bx wsk}
+
+echo Using CLI $WSK_CLI
 
 function deploy_visual_recognition {
-    # Deploy visual recognition action
     SOURCE_CLASSIFIER="actions/GetClassifier/Sources/Action/GetClassifier.swift"
     $WSK_CLI action update get-classifier --main classifyImage "$SOURCE_CLASSIFIER" --kind swift:4.1 --web true
-    $WSK_CLI service bind watson-vision-combined get-classifier
-    #$WSK_CLI action get get-classifier
+    if [ "$WSK_CLI" = "bx wsk" ]; then
+        $WSK_CLI service bind watson-vision-combined get-classifier
+    else
+        $WSK_CLI action update get-classifier -p apiKey "$WATSON_VISUAL_RECOGNITION_IAM_API_KEY"
+    fi
+
 }
 
 function deploy_reply_sms {
-    source secrets.env
-    # Deploy post sms message action
     SOURCE_REPLY_SMS="actions/GetClassifier/Sources/Action/ReplySMSMessage.swift"
     $WSK_CLI action update reply-sms --main replySMSMessage  "$SOURCE_REPLY_SMS" --kind swift:4.1 --web true
-    #$WSK_CLI action get reply-sms
 }
 
 function deploy_sequence {
@@ -34,12 +37,12 @@ function deploy_post_sms {
     #$WSK_CLI action get post-sms
 }
 
-
-
+#main functions
 deploy_visual_recognition
-#deploy_post_sms
 deploy_reply_sms
 deploy_sequence
+# For Bonus
+#deploy_post_sms
 
 
 
